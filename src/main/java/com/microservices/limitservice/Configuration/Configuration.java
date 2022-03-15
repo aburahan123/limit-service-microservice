573397b603349d4c670a9cada98a9086c2852c65
Abu Rahan <arahan@altimetrik.com>
2022-03-15 23:41:56 +0530

    first commit

---
+++ src/main/java/com/microservices/limitservice/Configuration/Configuration.java
@@ -0,0 +1,17 @@
+package com.microservices.limitservice.Configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@ConfigurationProperties("limit-service")
+@Getter
+@Setter
+public class Configuration {
+
+	private int minimum;
+	private int maximum;
+}
